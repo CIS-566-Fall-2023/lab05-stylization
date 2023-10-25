5d8a104320d6771ceb93ec94ea034a144c1beaae
Tongwei Dai <tongweid@gmail.com>
2023-10-25 12:45:01 -0400

    submit

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -20,11 +20,50 @@ void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction,
 #endif
 }
 
+void GetAdditionalLight_float(float3 WorldPos, out float3 Color, out float3 Direction, out float DistanceAtten, out float ShadowAtten) {
+#ifdef SHADERGRAPH_PREVIEW
+    Direction = normalize(float3(0.5, 0.5, 0));
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+    #if SHADOWS_SCREEN
+        float4 clipPos = TransformWorldToClip(WorldPos);
+        float4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+        float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+
+    int lightIndex = GetAdditionalLightsCount() - 1;
+    Light additionalLight = GetAdditionalLight(lightIndex, shadowCoord);
+    Direction = additionalLight.direction;
+    Color = additionalLight.color;
+    DistanceAtten = additionalLight.distanceAttenuation;
+    ShadowAtten = additionalLight.shadowAttenuation;
+#endif
+}
+
 void ChooseColor_float(float3 Highlight, float3 Shadow, float Diffuse, float Threshold, out float3 OUT)
 {
     if (Diffuse < Threshold)
     {
         OUT = Shadow;
+    }
+    else
+    {
+        OUT = Highlight;
+    }
+}
+
+void ChooseColor2_float(float3 Highlight, float3 Shadow, float3 Midtone, float Diffuse, float2 Thresholds, out float3 OUT)
+{
+    if (Diffuse < Thresholds.x)
+    {
+        OUT = Shadow;
+    }
+    else if (Diffuse < Thresholds.y)
+    {
+        OUT = Midtone;
     }
     else
     {
